5762738206528948663fe09ca0d4b8c83f342b16
Filipe Catarino <142268533+F-catarino@users.noreply.github.com>
2024-01-30 21:37:58 +0000

    Add files via upload
    Exercicio da hora e data atual

---
+++ Data e hora/script.js
@@ -0,0 +1,14 @@
+const dataAtual = new Date();
+
+console.log(dataAtual);
+
+
+let dia = dataAtual.getDate()
+let mes = dataAtual.getMonth() + 1
+let ano = dataAtual.getFullYear()
+let hora = dataAtual.getHours()
+let minutos = dataAtual.getMinutes()
+
+let dataAtualFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minutos
+
+document.getElementById("currentDateTime").innerHTML = dataAtualFormatada;
